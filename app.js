93904e194691e81fe5ff8a7c25ecb164a723f666
Addison <addisonbennett@gmail.com>
2015-11-16 16:20:40 -0700

    added USD to BTC

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 $(function() {
   console.log($.fn.jquery);
   var bitCoinInput= document.querySelector('.bitCoinAmount');
-  // parseFloat (Math.round(bitCoinAmount * 100) / 100).toFixed(2);
+  var USDInput= document.querySelector('.USDAmount');
+  var currentRate
+   $.post( "https://cex.io/api/convert/BTC/USD", {amnt: "1.0"}, function( data ) {
+     console.log(data);
+     currentRate=data.amnt
+  });
   var bitCoinAmount= bitCoinInput.value;
   bitCoinInput.addEventListener("keyup", function(){
     var bitCoinAmount= bitCoinInput.value;
@@ -10,13 +15,11 @@ $(function() {
        appendRateToUSD(data);
     });
   });
-var USDInput= document.querySelector('.USDAmount');
-USDInput.addEventListener("keyup", function(){
+USDInput.addEventListener("keyup", function(data){
   var USDAmount= USDInput.value;
-  $.post( "https://cex.io/api/convert/USD/BTC", {amnt: bitCoinAmount}, function( data ) {
-    //  console.log(data);
+  (currentRate * USDAmount);
      appendRateToBTC(data);
-  });
+
 });
 
 var appendRateToUSD = function(data){
